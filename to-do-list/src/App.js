1d492144505104461acb18eeb511f979b58e5ef0
AdityaRaskar <adr532003@gmail.com>
2024-02-24 18:25:10 +0530

    Most of part Completed

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState } from 'react'
+
 
 function App() {
+
+  let [todolist,settodolist]=useState([])
+
+  let saveTodoList=(event)=>{
+
+    let todoname=event.target.todoname.value;
+    
+    // alert(todoname)
+    if(!todolist.includes(todoname)){
+      let finalTodolist=[...todolist,todoname]
+      settodolist(finalTodolist)
+    }
+    else{
+      alert("Already Exist with same name")
+    }
+    event.preventDefault(); // stops page refreshing on save btn press
+  }
+
+
+  let list=todolist.map((value,index)=>{
+    return(
+      <TodoListItems value={value} key={index} indexNum={index}
+      todolist={todolist}
+      settodolist={settodolist}
+      />
+    )
+  }
+  )
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <h1 className='title'>To do list</h1>
+
+      <div className="outer">
+      <form onSubmit={saveTodoList}>
+        <input type="text" name="todoname" />
+        <button>Save</button>
+      </form>
+
+      <div className="list">
+        <ul>
+          {list}
+        </ul>
+      </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+function TodoListItems({value ,indexNum ,todolist,settodolist}){
+
+  let deleteRow=()=>{
+    // alert(`${indexNum}`)
+    let finalTodolist_del = todolist.filter((v,i)=>i!==indexNum)
+    settodolist(finalTodolist_del)
+
+  }
+
+  let [status,setstatus]=useState(true)
+
+  let statusCheck=()=>{
+    setstatus(!status)
+  }
+  return(
+    <li class={(status) ? "" : "strikeList"} onClick={statusCheck}>{indexNum+1} {value}<span onClick={deleteRow}>&times;</span></li>
+    )
+}
